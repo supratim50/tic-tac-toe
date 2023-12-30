ef2ec327d8ad3d5802b52da80cdddabe8b059416
Supratim Mondal <supratimmondal50@gmail.com>
2023-12-30 23:06:55 +0530

    fixed infinite marking

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const gameBoxes = document.querySelectorAll(".gameBox");
+const gameoverElement = document.getElementById("gameover");
+const messageElement = document.getElementById("message");
+
 let emptySquare = [1,2,3,4,5,6,7,8,9];
+let totalChances = 9;
+
+let userOneScore = 0;
+let markByUserOne = [];
+
+let userTwoScore = 0;
+let markByUserTwo = [];
+
+let gameOver = false;
 
 const markOnTheSquare = (markableBox, mark) => {
     if(mark == "cross") {
@@ -8,28 +20,39 @@ const markOnTheSquare = (markableBox, mark) => {
         markableBox.classList.add("active-nought");
     }
 }
-const removeEmptySquare = (removableBox) => {
-    let boxId = removableBox.getAttribute("id");
-    let newEmptySquare = emptySquare.filter(square => square != Number(boxId));
+const removeEmptySquare = (removableBoxId) => {
+    let newEmptySquare = emptySquare.filter(square => square != Number(removableBoxId));
     emptySquare = newEmptySquare;
 }
 
 gameBoxes.forEach((gameBox) => {
     gameBox.addEventListener("click", () => {
-        markOnTheSquare(gameBox, "cross") //TODO: need to chnage the constant value "cross"
-        removeEmptySquare(gameBox);
+        markByUser(gameBox);
         setTimeout(() => {
             compGenerateMark();
         }, 500)
     })
 })
 
+const markByUser = (gameBox) => {
+    if(totalChances == 0 || gameOver == true) return
+
+    markOnTheSquare(gameBox, "cross") //TODO: need to chnage the constant value "cross"
+    let boxId = gameBox.getAttribute("id");
+    markByUserOne.push(boxId);
+    removeEmptySquare(boxId);
+    totalChances -= 1;
+}
+
 const compGenerateMark = () => {
+    if(totalChances == 0 || gameOver == true) return
+
     const randomNumber = Math.floor(Math.random() * emptySquare.length);
     const squareNumber = emptySquare[randomNumber];
-
     //* Mark the square
     const gameBox = document.getElementById(`${squareNumber}`);
     markOnTheSquare(gameBox, "nought") //TODO: need to chnage the constant value "cross"
-    removeEmptySquare(gameBox);
+    markByUserTwo.push(squareNumber);
+    removeEmptySquare(squareNumber);
+    totalChances -= 1;
 }
